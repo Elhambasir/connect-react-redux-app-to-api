a173752831d08d203910a19d48411b8c807f1df9
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-18 19:47:31 +0430

    Create store.jsx

---
+++ src/store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import usersSlice from './store/users/usersSlice';
+const store = configureStore({
+  reducer: {
+     users: usersSlice,
+  },
+})
+
+export default store;
